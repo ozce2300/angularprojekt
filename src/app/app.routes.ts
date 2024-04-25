6e2b97939f6b37d74914dec597f43da8702bbe30
Özgur Celik <ozce2300@student.miun.se>
2024-04-25 02:50:11 +0200

    Skapat grunder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ConvertComponent } from './convert/convert.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "home", component: HomeComponent},
+    {path: "convert", component: ConvertComponent},
+    {path: "about", component: AboutComponent},
+    {path: "", redirectTo: "/home", pathMatch: "full"},
+    {path: "404", component: NotFoundComponent},
+    {path: "**", component: NotFoundComponent}
+
+];
